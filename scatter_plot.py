09a455f116027912ae20e79543784a48538f7304
Ayrat Mustaev <aimustaev@avito.ru>
2021-07-18 02:12:34 +0400

    add chaprer_1_2

---
+++ scatter_plot.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import sys
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from libs import snackbar
+from read import read_csv
+
+
+def preprocess():
+    if len(sys.argv) == 1:
+        snackbar("Set file name", 'error')
+    df = read_csv(sys.argv[1])
+    targert_col = 'Hogwarts House'
+    if (targert_col not in df.keys()):
+        snackbar("Your dataset don't have Hogwarts House", 'error')
+
+    corr = df.corr()
+    mask = np.zeros_like(corr)
+    mask[np.triu_indices_from(mask)] = True
+    plt.figure(figsize=(10, 10))
+    sns.heatmap(corr, mask=mask, annot=True)
+    plt.show()
+
+    plt.figure(figsize=(10, 10))
+    feat1 = 'Astronomy'
+    feat2 = 'Defense Against the Dark Arts'
+    target_col = 'Hogwarts House'
+    sns.scatterplot(data=df, x=feat1, y=feat2, hue=target_col)
+    plt.show()
+
+
+def main():
+    preprocess()
+
+
+if __name__ == '__main__':
+    main()
